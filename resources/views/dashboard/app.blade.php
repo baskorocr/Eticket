03b5fd6e66400d98d4aa9b3ee5fed3eb067324ef
Mahesa <baskorocr@gmail.com>
2024-06-13 15:36:59 +0700

    updates new

--- resources/views/dashboard/app.blade.php
+++ resources/views/dashboard/app.blade.php
@@ -20,19 +20,19 @@
     {{ asset('assets/css/fontawesome.css') }}
     
     <!-- Bootstrap Core CSS -->
-    <link href=" {{ asset('../assets/node_modules/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href=" {{ asset('../assets/node_modules/perfect-scrollbar/css/perfect-scrollbar.css') }}" rel="stylesheet">
+    <link href="../assets/node_modules/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/node_modules/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet">
     <!-- This page CSS -->
     <!-- chartist CSS -->
-    <link href=" {{ asset('../assets/node_modules/morrisjs/morris.css') }}" rel="stylesheet">
+    <link href="../assets/node_modules/morrisjs/morris.css" rel="stylesheet">
     <!--c3 CSS -->
-    <link href=" {{ asset('../assets/node_modules/c3-master/c3.min.css') }}" rel="stylesheet">
+    <link href="../assets/node_modules/c3-master/c3.min.css" rel="stylesheet">
     <!-- Custom CSS -->
-    <link href=" {{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
     <!-- Dashboard 1 Page CSS -->
-    <link href=" {{ asset('css/pages/dashboard1.css') }}" rel="stylesheet">
+    <link href="css/pages/dashboard1.css" rel="stylesheet">
     <!-- You can change the theme colors from here -->
-    <link href=" {{ asset('css/colors/default.css') }}" id="theme" rel="stylesheet">
+    <link href="css/colors/default.css" id="theme" rel="stylesheet">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -197,29 +197,28 @@
     <!-- All Jquery -->
     <!-- ============================================================== -->
    @vite('resources/js/app.js')
-   
-    <script src="{{ asset('../assets/node_modules/jquery/jquery.min.js') }}"></script>
+    <script src="../assets/node_modules/jquery/jquery.min.js"></script>
     <!-- Bootstrap popper Core JavaScript -->
-    <script src="{{ asset('../assets/node_modules/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="../assets/node_modules/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- slimscrollbar scrollbar JavaScript -->
-    <script src="{{ asset('js/perfect-scrollbar.jquery.min.js') }}"></script>
+    <script src="js/perfect-scrollbar.jquery.min.js"></script>
     <!--Wave Effects -->
-    <script src="{{ asset('js/waves.js') }}"></script>
+    <script src="js/waves.js"></script>
     <!--Menu sidebar -->
-    <script src="{{ asset('js/sidebarmenu.js') }}"></script>
+    <script src="js/sidebarmenu.js"></script>
     <!--Custom JavaScript -->
-    <script src="{{ asset('js/custom.min.js') }}"></script>
+    <script src="js/custom.min.js"></script>
     <!-- ============================================================== -->
     <!-- This page plugins -->
     <!-- ============================================================== -->
     <!--morris JavaScript -->
-    <script src="{{ asset('../assets/node_modules/raphael/raphael-min.js') }}"></script>
-    <script src="{{ asset('../assets/node_modules/morrisjs/morris.min.js') }}"></script>
+    <script src="../assets/node_modules/raphael/raphael-min.js"></script>
+    <script src="../assets/node_modules/morrisjs/morris.min.js"></script>
     <!--c3 JavaScript -->
-    <script src="{{ asset('../assets/node_modules/d3/d3.min.js') }}"></script>
-    <script src="{{ asset('../assets/node_modules/c3-master/c3.min.js') }}"></script>
+    <script src="../assets/node_modules/d3/d3.min.js"></script>
+    <script src="../assets/node_modules/c3-master/c3.min.js"></script>
     <!-- Chart JS -->
-    <script src="{{ asset('js/dashboard1.js') }}"></script>
+    <script src="js/dashboard1.js"></script>
 </body>
 
 </html>
